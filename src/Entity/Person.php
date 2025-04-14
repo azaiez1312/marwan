ebc87017b1d7ad554e88e88ccf6fdd5c55df7270
Marwan <azaiezmarwan92@gmail.com>
2025-04-14 14:59:05 +0100

    suuu

--- src/Entity/Person.php
+++ src/Entity/Person.php
@@ -6,15 +6,83 @@ use App\Repository\PersonRepository;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: PersonRepository::class)]
-class Person
+#[ORM\InheritanceType('JOINED')]
+#[ORM\DiscriminatorColumn(name: 'discr', type: 'string')]
+#[ORM\DiscriminatorMap([
+    'person' => Person::class,
+    'staff' => Staff::class,
+    'resident' => Resident::class
+])]
+abstract class Person
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
     private ?int $id = null;
 
+    #[ORM\Column(length: 255)]
+    private string $firstName;
+
+    #[ORM\Column(length: 255)]
+    private string $lastName;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $email = null;
+
+    #[ORM\Column(length: 20, nullable: true)]
+    private ?string $phone = null;
+
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getFirstName(): string
+    {
+        return $this->firstName;
+    }
+
+    public function setFirstName(string $firstName): self
+    {
+        $this->firstName = $firstName;
+        return $this;
+    }
+
+    public function getLastName(): string
+    {
+        return $this->lastName;
+    }
+
+    public function setLastName(string $lastName): self
+    {
+        $this->lastName = $lastName;
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(?string $email): self
+    {
+        $this->email = $email;
+        return $this;
+    }
+
+    public function getPhone(): ?string
+    {
+        return $this->phone;
+    }
+
+    public function setPhone(?string $phone): self
+    {
+        $this->phone = $phone;
+        return $this;
+    }
+
+    public function getFullName(): string
+    {
+        return $this->firstName . ' ' . $this->lastName;
     }
 }
--- src/Entity/Staff.php
+++ src/Entity/Staff.php
@@ -6,15 +6,66 @@ use App\Repository\StaffRepository;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: StaffRepository::class)]
-class Staff
+class Staff extends Person
 {
-    #[ORM\Id]
-    #[ORM\GeneratedValue]
-    #[ORM\Column]
-    private ?int $id = null;
+    #[ORM\Column(length: 100)]
+    private string $position;
 
-    public function getId(): ?int
+    #[ORM\Column(length: 50)]
+    private string $department;
+
+    #[ORM\Column(type: 'date')]
+    private \DateTimeInterface $hireDate;
+
+    #[ORM\Column(type: 'decimal', precision: 10, scale: 2, nullable: true)]
+    private ?string $salary = null;
+
+    public function getPosition(): string
     {
-        return $this->id;
+        return $this->position;
+    }
+
+    public function setPosition(string $position): self
+    {
+        $this->position = $position;
+        return $this;
+    }
+
+    public function getDepartment(): string
+    {
+        return $this->department;
+    }
+
+    public function setDepartment(string $department): self
+    {
+        $this->department = $department;
+        return $this;
+    }
+
+    public function getHireDate(): \DateTimeInterface
+    {
+        return $this->hireDate;
+    }
+
+    public function setHireDate(\DateTimeInterface $hireDate): self
+    {
+        $this->hireDate = $hireDate;
+        return $this;
+    }
+
+    public function getSalary(): ?string
+    {
+        return $this->salary;
+    }
+
+    public function setSalary(?string $salary): self
+    {
+        $this->salary = $salary;
+        return $this;
+    }
+
+    public function getYearsOfService(): int
+    {
+        return (new \DateTime())->diff($this->hireDate)->y;
     }
 }
