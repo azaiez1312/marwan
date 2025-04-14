0e10cc09297a4fd628b8919cd3f7a0ce97406a21
Marwan <azaiezmarwan92@gmail.com>
2025-04-14 17:25:33 +0100

    Updated project with improved files

--- src/Entity/Visite.php
+++ src/Entity/Visite.php
@@ -13,23 +13,106 @@ class Visite
     #[ORM\Column]
     private ?int $id = null;
 
+    #[ORM\Column(type: 'datetime')]
+    private \DateTimeInterface $date;
+
+    #[ORM\Column(length: 255)]
+    private string $visitType;
+
+    #[ORM\Column(type: 'text')]
+    private string $reason;
+
+    #[ORM\Column(type: 'text', nullable: true)]
+    private ?string $notes = null;
+
+    #[ORM\Column(type: 'datetime')]
+    private \DateTimeInterface $createdAt;
+
+    #[ORM\Column(type: 'datetime')]
+    private \DateTimeInterface $updatedAt;
+
     #[ORM\ManyToOne(inversedBy: 'visites')]
-    private ?medicalrecord $medicalrecord = null;
+    private ?Medicalrecord $medicalrecord = null;
 
     public function getId(): ?int
     {
         return $this->id;
     }
 
-    public function getMedicalrecord(): ?medicalrecord
+    public function getDate(): \DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTimeInterface $date): static
+    {
+        $this->date = $date;
+        return $this;
+    }
+
+    public function getVisitType(): string
+    {
+        return $this->visitType;
+    }
+
+    public function setVisitType(string $visitType): static
+    {
+        $this->visitType = $visitType;
+        return $this;
+    }
+
+    public function getReason(): string
+    {
+        return $this->reason;
+    }
+
+    public function setReason(string $reason): static
+    {
+        $this->reason = $reason;
+        return $this;
+    }
+
+    public function getNotes(): ?string
+    {
+        return $this->notes;
+    }
+
+    public function setNotes(?string $notes): static
+    {
+        $this->notes = $notes;
+        return $this;
+    }
+
+    public function getCreatedAt(): \DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $createdAt): static
+    {
+        $this->createdAt = $createdAt;
+        return $this;
+    }
+
+    public function getUpdatedAt(): \DateTimeInterface
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(\DateTimeInterface $updatedAt): static
+    {
+        $this->updatedAt = $updatedAt;
+        return $this;
+    }
+
+    public function getMedicalrecord(): ?Medicalrecord
     {
         return $this->medicalrecord;
     }
 
-    public function setMedicalrecord(?medicalrecord $medicalrecord): static
+    public function setMedicalrecord(?Medicalrecord $medicalrecord): static
     {
         $this->medicalrecord = $medicalrecord;
-
         return $this;
     }
 }
