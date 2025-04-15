da40367b019c2877d247cba4cb8d97beb1a967d1
Marwan <azaiezmarwan92@gmail.com>
2025-04-15 17:59:04 +0100

    SUUU

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -1,15 +1,22 @@
 <?php
-
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
-class IndexController extends AbstractController
+class PageController extends AbstractController
 {
+    // Route for the homepage (index.html.twig)
     #[Route('/', name: 'index')]
     public function index()
     {
         return $this->render('index.html.twig');
     }
+    #[Route('/resident', name: 'resident')]
+    public function resident()
+    {
+        return $this->render('resident.html.twig');
+    }
+    
+
 }
